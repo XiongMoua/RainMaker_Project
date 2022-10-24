9680ce540d7393ff5bc1b7f8f1ae237ad6197fb6
Xiong Moua <76863290+XiongMoua@users.noreply.github.com>
2022-10-24 14:46:37 -0700

    Setting up the all the classes

---
+++ src/GameApp.java
@@ -0,0 +1,37 @@
+class Game{
+
+}
+
+class GameObject{
+
+}
+
+class Pond{
+
+}
+
+class Cloud{
+
+}
+
+class Helipad{
+
+}
+
+class Helicopter{
+
+}
+
+class PondAndCloud{
+
+}
+
+interface Updatable{
+
+}
+
+public class GameApp {
+ public static void main(String[]args){
+  System.out.println("Hello world");
+ }
+}
